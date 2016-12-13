93e47bb95f93b5e449116aa2876998270c5ce75b
mattcheno <matthew.william.chenoweth@gmail.com>
2016-12-13 11:01:31 -0600

    Test for non-numeric Meter field
    Test for non-numeric Meter field value in other-wise successful observations

--- ebsCSVsnake.py
+++ ebsCSVsnake.py
@@ -37,6 +37,7 @@ m = 0  #counter, percentage status
 e1 = 0  #counter, 'ModelCode contains NA value'
 e2 = 0  #counter, 'Manufacturer not found in Key File'
 e3 = 0  #counter, 'Model not found in Key File'
+e4 = 0  #counter, 'Meter is not numeric'
 z = 0  #counter, number of zero Meter readings
 nullRgX = re.compile(r'unk.*|(x){2,}|N/A', re.I) #Null-Value RegEx
 dashRgX = re.compile(r'-|/') #Dashes or Slashes RegEx
@@ -114,13 +115,16 @@ for row in ebsReader:
 		exWriter.writerow(row)
 		continue
 	else:
+		try:  #----------------------------------------------Footnote 002
+			if row[9] < 1: z = z + 1    # row[9] is Meter reading
+		except TypeError
+			row.append('Meter is not numeric')
+			e4 = e4 + 1
+			continue
 		row[5] = newManf
 		row[16] = uType     # row[16] is 'Class'
 		outputWriter.writerow(row)
 		j = j + 1
-		if row[9] < 1:     # row[9] is Meter reading
-			z = z + 1
-		continue
 	
 #end of for loop
 
@@ -137,7 +141,9 @@ runStats = ('Complete: ' + str(round(100 * j / k, 4)) +
 	str(e2) + ' (' + str(round(100 * e2 / k, 4)) +
 	'%) :: Manufacturer not found in Key File\n' +
 	str(e3) + ' (' + str(round(100 * e3 / k, 4)) +
-	'%) :: Model not found in Key File\n==========\n' +
+	'%) :: Model not found in Key File\n' +
+	str(e4) + ' (' + str(round(100 * e4 / k, 4)) +
+	'%) :: Meter not numeric\n==========\n' +
 	str(z) + ' [' + str(round(100 * z / j, 4)) +
 	'% of Complete] :: Zero Meter Value\n==========\n' +
 	str(round(time.time() - tStart, 4 )) + ' Total Seconds Runtime')
@@ -152,4 +158,6 @@ print(runStats)
 # Look up the three-digit MFG code (row[5]) from the observation in the UberKey
 # dictionary and assign the nested dictionary of models/unit-types to makeDict
 # variable. Assign "ERR01" code if MFG code not found.
+#    Note 002
+# Test for non-numeric Meter field value in other-wise successful observations
 #==============================================================END OF CODE=====
